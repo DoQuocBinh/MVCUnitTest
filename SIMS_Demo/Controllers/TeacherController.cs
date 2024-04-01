4f87918e04c77ad2f183fd6aec76b6be92dd1272
DoQuocBinh <binhdq@fpt.edu.vn>
2024-04-01 16:07:32 +0700

    ok

---
+++ SIMS_Demo/Controllers/TeacherController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SIMS_Demo.Models;
+
+
+namespace SIMS_Demo.Controllers
+{
+    public class TeacherController : Controller
+    {
+        static List<Teacher>? teachers = new List<Teacher>();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            teachers = ReadFileToTeacherList("data.json");
+            return View(teachers);
+        }
+
+        public static List<Teacher>? ReadFileToTeacherList(String filePath)
+        {
+            // Read a file
+            string readText = System.IO.File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Teacher>>(readText);
+        }
+
+        //click Hyperlink
+        [HttpGet]
+        public IActionResult NewTeacher()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewTeacher(Teacher teacher)
+        {
+            teachers.Add(teacher);
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(teachers, options);
+            using (StreamWriter writer = new StreamWriter("data.json"))
+            {
+                writer.Write(jsonString);
+            }
+            return Content(jsonString);
+        }
+    }
+}
---
+++ SIMS_Demo/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SIMS_Demo.Models
+{
+	public class Teacher
+	{
+		public int Id { get; set; }
+		public String Name { get; set; }
+		public DateTime DoB { get; set; }
+		public Teacher()
+		{
+		}
+	}
+}
+
